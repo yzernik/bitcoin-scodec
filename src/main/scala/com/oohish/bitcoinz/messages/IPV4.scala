dbf44dbe7d817114cc9b06f3a89e0a864f18f96e
yzernik <yzernik@gmail.com>
2014-09-01 18:33:13 -0700

    improved ipv4 tests

--- src/main/scala/com/oohish/bitcoinz/messages/IPV4.scala
+++ src/main/scala/com/oohish/bitcoinz/messages/IPV4.scala
@@ -16,5 +16,17 @@ case class IPV4(value: Int) extends IP {
 
 object IPV4 {
 
+  /** Creates an IPV4 from ip fields. */
+  def apply(f1: Int, f2: Int, f3: Int, f4: Int): IPV4 =
+    IPV4((f1 << 24) + (f2 << 16) + (f3 << 8) + f4)
+
+  /** Creates an IPV4 from string. */
+  def apply(s: String): IPV4 = {
+    val fields = s.split('.')
+    assert(fields.length == 4)
+    val digs = fields.map(_.toInt)
+    IPV4(digs(0), digs(1), digs(2), digs(3))
+  }
+
   implicit val codec: Codec[IPV4] = codecs.int32.xmap[IPV4](v => IPV4(v), _.value)
 }
--- src/test/scala/com/oohish/bitcoinz/messages/IPV4Spec.scala
+++ src/test/scala/com/oohish/bitcoinz/messages/IPV4Spec.scala
@@ -14,22 +14,24 @@ class IPV4Spec extends CodecSuite {
   "IPV4 codec" should {
     "roundtrip" in {
       roundtrip(IPV4(1234))
+      roundtrip(IPV4("10.0.0.1"))
       roundtrip(Ping(Long.MinValue))
       roundtrip(Ping(Long.MaxValue))
     }
 
     "print" in {
-      IPV4((10 << 24) + 1).toString shouldBe "10.0.0.1"
+      IPV4(10, 0, 0, 1).toString shouldBe "10.0.0.1"
+      IPV4("10.0.0.1").toString shouldBe "10.0.0.1"
     }
 
     "encode" in {
-      codec.encode(IPV4((10 << 24) + 1)) shouldBe
+      codec.encode(IPV4("10.0.0.1")) shouldBe
         \/.right(hex"0A 00 00 01".toBitVector)
     }
 
     "decode" in {
       codec.decode(hex"0A 00 00 01".toBitVector) shouldBe
-        \/.right(BitVector.empty, IPV4((10 << 24) + 1))
+        \/.right(BitVector.empty, IPV4("10.0.0.1"))
     }
   }
 }
