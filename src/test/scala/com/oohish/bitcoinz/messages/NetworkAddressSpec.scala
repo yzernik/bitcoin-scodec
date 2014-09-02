ce70930af23de67ff05d3572266f5af70154d13c
yzernik <yzernik@gmail.com>
2014-09-01 20:59:43 -0700

    improved network address test

--- src/test/scala/com/oohish/bitcoinz/messages/NetworkAddressSpec.scala
+++ src/test/scala/com/oohish/bitcoinz/messages/NetworkAddressSpec.scala
@@ -13,14 +13,15 @@ class NetworkAddressSpec extends CodecSuite {
 
   "NetworkAddress codec" should {
     "roundtrip" in {
-      roundtrip(NetworkAddress(1234, Left(IPV4(333)), Port(8080)))
+      roundtrip(NetworkAddress(1234, Left(IPV4("10.0.0.1")), Port(8080)))
+      roundtrip(NetworkAddress(1234, Right(IPV6(hex"2001 0db8 85a3 0042 1000 8a2e 0370 7334")), Port(8080)))
       roundtrip(NetworkAddress(Long.MinValue, Left(IPV4(Int.MinValue)), Port(0)))
       roundtrip(NetworkAddress(Long.MaxValue, Left(IPV4(Int.MaxValue)), Port(65535)))
     }
 
     "encode" in {
       val services = 1L
-      val ip = Left(IPV4((10 << 24) + 1))
+      val ip = Left(IPV4("10.0.0.1"))
       val port = Port(8333)
 
       codec.encode(NetworkAddress(services, ip, port)) shouldBe
