18273fde5f0b8b579f82387463a73009589ea3c0
yzernik <yzernik@gmail.com>
2014-09-02 21:02:49 -0700

    fixed encode function for VarInt

--- src/main/scala/com/oohish/bitcoinscodec/structures/VarInt.scala
+++ src/main/scala/com/oohish/bitcoinscodec/structures/VarInt.scala
@@ -8,16 +8,28 @@ object VarInt {
   import scodec.codecs._
 
   val varIntCodec = Codec[Long](
-    (n: Long) =>
-
-      n match {
-        case i: Long if (i < 0xfd) => uint8.encode(n.toByte)
-        case i: Long if (i < 0xffff) => uint16.encode(n.toByte)
-        case i: Long if (i < 0xffffffff) => uint32.encode(n.toByte)
-        case i: Long => uint32.encode(n.toByte)
-      },
+    (n: Long) => n match {
+      case i: Long if (i < 0xfd) =>
+        uint8.encode(i.toByte)
+      case i: Long if (i < 0xffff) =>
+        for {
+          a <- uint8.encode(0xfd.toByte)
+          b <- uint16.encode(i.toByte)
+        } yield a ++ b
+      case i: Long if (i < 0xffffffff) =>
+        for {
+          a <- uint8.encode(0xfe.toByte)
+          b <- uint32.encode(i.toByte)
+        } yield a ++ b
+      case i: Long =>
+        for {
+          a <- uint8.encode(0xff.toByte)
+          b <- Codec[UInt64].encode(UInt64(UInt64.bigIntToLong(BigInt(i))))
+        } yield a ++ b
+    },
     (buf: BitVector) => {
       val y = uint32.decode(buf)
+
       y
     })
 
