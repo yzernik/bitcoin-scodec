7086589f8d8b747986dff2cc01e25616459a81b5
Jonathan Zernik <yzernik@gmail.com>
2019-09-17 14:12:53 -0700

    Migrato to Scala 2.12

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,7 +1,5 @@
 resolvers += Classpaths.sbtPluginReleases
 
-addSbtPlugin("org.scoverage" %% "sbt-scoverage" % "0.99.11")
+addSbtPlugin("org.scoverage" %% "sbt-scoverage" % "1.6.0")
 
-addSbtPlugin("org.scoverage" %% "sbt-coveralls" % "0.99.0")
-
-addSbtPlugin("me.lessis" % "bintray-sbt" % "0.3.0")
+addSbtPlugin("org.scoverage" %% "sbt-coveralls" % "1.2.7")
--- src/test/scala/io/github/yzernik/bitcoinscodec/messages/HeadersSpec.scala
+++ src/test/scala/io/github/yzernik/bitcoinscodec/messages/HeadersSpec.scala
@@ -1,10 +1,14 @@
 package io.github.yzernik.bitcoinscodec.messages
 
+import java.nio.file.{Files, Paths}
+
 import io.github.yzernik.bitcoinscodec.CodecSuite
 import scodec._
 import scodec.bits._
 import scodec.codecs._
 import io.github.yzernik.bitcoinscodec.structures._
+
+import scala.io.Source
 
 class HeadersSpec extends CodecSuite {
 
@@ -16,10 +20,11 @@ class HeadersSpec extends CodecSuite {
     }
 
     "decode" in {
-      val buf = scalax.io.Resource.fromURL(getClass.getResource("/headerspayload.data"))
-      val bytes = BitVector(buf.bytes)
+      val resource = getClass.getResource("/headerspayload.data")
+      val bytes = Files.readAllBytes(Paths.get(resource.getPath))
+      val bitVector = BitVector(bytes)
 
-      val Attempt.Successful(DecodeResult(actual, rest)) = Headers.codec(1) decode bytes
+      val Attempt.Successful(DecodeResult(actual, rest)) = Headers.codec(1) decode bitVector
       rest shouldBe BitVector.empty
     }
 
